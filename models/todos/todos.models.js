4b713f65bdab099d965eb6157acdfb383f9ccb7d
Divyakumar0711 <divyakumarapatel0711@gmail.com>
2024-12-26 09:24:08 -0500

    Todos

---
+++ models/todos/todos.models.js
@@ -0,0 +1,22 @@
+import { type } from "express/lib/response";
+import mongoose from "mongoose";
+const todoSchema = new mongoose.Schema({
+    content:{
+        type:String,
+        require:true,
+    },
+    checked:{
+        type:Boolean,
+        default:false
+    },
+    createdBy:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+    subTodos:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"SubTodo"
+    }]
+}, {timestamps:true})
+
+export const Todo = mongoose.model("Todo", todoSchema)
